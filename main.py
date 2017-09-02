3589e8c7bc3bdd532aacb3fa7887947912fdb4ad
Adam Hecht <ahecht@MacBook-Pro.local>
2017-09-02 18:13:39 -0400

    First commit for lift tracker app. Initial routes for lift and date.

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+app = Flask(__name__)
+import re
+
+@app.route('/lift/<lift>', methods = ['GET', 'POST'])
+def lift_type(lift):
+    success_post = request.get_json(force=True)
+    if request.method == 'POST':
+        return success_post
+    else:
+        return 'Lift = %s' % lift
+
+@app.route('/date/<date>')
+def date_check(date):
+    if re.match('\d{4}-\d{1,2}-\d{1,2}', date):
+        return 'Date = %s' % date
+    else:
+        return 'Date invalid, please use "YYYY-MM-DD" format'
+
+# The next step is to add sets, reps, weight to the lift_type function.
+# We want the lift_type function to be a POST only
+# We want the date_check to be GET only
+# See Slack for what the eventual JSON response will be for lift_type
