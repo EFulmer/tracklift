04df7f5c20308649878ac9edf1eee62846a4b3d4
Adam Hecht <ahecht@MacBook-Pro.local>
2017-09-18 12:42:51 -0400

    Added some tests to Create rows

--- real_test.py
+++ real_test.py
@@ -20,14 +20,16 @@ def connect ():
         # create a cursor
         cur = conn.cursor()
 
-        Session = sessionmaker(db)
+        Session = sessionmaker(conn)
         session = Session()
-# insert data
-        leg_day = Workouts(id=0, Date =  
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print (db_version)
+# insert data into the table
+        leg_day = Workouts(id=0, Date = 1111-22-33)
+        squats = Lifts(id=0, workout=0, warm_up=True, name= "Squat", lift_ord=1,
+                workout_ord=1)
+        set_rep = Sets(id=0, lift=0, set_count=1, rep_count=1, weight=200, lift_ord =1)
+        session.add(leg_day)
+        session.add(squats)
+        session.add(set_rep)
 
     # close connection with server
         cur.close()
