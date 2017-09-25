c2052685203dcfbfebcc02c5603a4245d57e0157
Adam Hecht <ahecht@MacBook-Pro.local>
2017-09-25 09:07:17 -0400

    Odd output happening, please check

--- real_test2.py
+++ real_test2.py
@@ -18,10 +18,10 @@ session = Session()
 print ('Connecting to the PostgreSQL database...')
 
 # create
-leg_day = Workouts(id=0, day = '2017-2-3')
-squats = Lifts(id=0, workout=0, warm_up=True, name= "Squat", lift_ord=1,
+leg_day = Workouts(id=1, day = '2017-2-3')
+squats = Lifts(id=2, workout=0, warm_up=True, name= "Squat", lift_ord=1,
         workout_ord=1)
-set_rep = Sets(id=0, lift=0, set_count=1, rep_count=1, weight=200, lift_ord =1)
+set_rep = Sets(id=3, lift=0, set_count=1, rep_count=1, weight=200, lift_ord =1)
 session.add(leg_day)
 session.add(squats)
 session.add(set_rep)
@@ -29,11 +29,23 @@ session.add(set_rep)
 # read
 workouts = session.query(Workouts)
 for workout in workouts:
-    print(workout.id)
+    print(workout.day)
+
+swole = session.query(Lifts)
+for get in swole:
+    print(get.name)
+
+huge = session.query(Sets)
+for makeme in huge:
+    print(makeme.weight)
 
 # update
 set_rep.weight = 240
 session.commit
+
+updated = session.query(Sets)
+for updates in updated:
+    print(updates.weight)
 
 # delete
 session.delete(set_rep)
