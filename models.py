befe545d18ab6e291a71dd518ec160b2f2047453
Adam Hecht <ahecht@SMNY-ahecht-mac.fios-router.home>
2017-10-31 03:04:26 -0400

    Brought Sets model in line with DB

--- models.py
+++ models.py
@@ -10,33 +10,34 @@ from sqlalchemy.orm import relationship
 class Workouts(Base):
     __tablename__ = 'workouts'
     
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key = True, autoincrement = True)
     day = Column(Date, nullable = False)
 
-    get_pumped = relationship("Lifts", back_populates =  "session")
+    lifts_relate = relationship("Lifts", back_populates =  "session")
 
 class Lifts(Base):
     __tablename__ = 'lifts'
 
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key = True, autoincrement = True)
     workout = Column(Integer, ForeignKey('workouts.id'))
     warm_up = Column(Boolean, nullable = False)
     name = Column(Text, nullable = False)
     lift_ord = Column(Integer, nullable = False)
-    workout_ord = Column(Integer, nullable = False)
+#    workout_ord = Column(Integer, nullable = False)
 
-    session = relationship("Workouts", back_populates = "get_pumped")
-    milk_for_babies = relationship("Sets", back_populates = "lifts")
+    session = relationship("Workouts", back_populates = "lifts_relate")
+    sets_relate = relationship("Sets", back_populates = "lifts")
 
 class Sets(Base):
     __tablename__ = 'sets'
 
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key = True, autoincrement = True)
     lift = Column(Integer, ForeignKey('lifts.id'))
     set_count = Column(Integer, nullable = False)
     rep_count = Column(Integer, nullable = False)
     weight = Column(Integer, nullable = False)
+    warm_up = Column(Boolean, nullable = False)
     notes = Column(Text, nullable = True)
-    lift_ord = Column(Integer, nullable = False)
+    set_ord = Column(Integer, nullable = False)
 
-    lifts = relationship("Lifts", back_populates = "milk_for_babies")
+    lifts = relationship("Lifts", back_populates = "sets_relate")
