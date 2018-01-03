4b6e10708f1e80af0127a606f1c30c4d70e59104
Eric Fulmer <eric.r.fulmer@gmail.com>
2018-01-02 21:13:03 -0500

    update reqs with `testing.postgresql` and import it into `test.py`

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
 import unittest
 
+import testing.postgresql
+
 import main
 
+Postgresql = testing.postgresql.PostgresqlFactory(cache_initialized_db=True)
 
 class TrackliftTestCase(unittest.TestCase):
     def setUp(self):
         self.app = main.app.test_client()
+        self.postgresql = Postgresql()
 
     def tearDown(self):
-        pass
+        self.postgresql.stop()
     
     def test_get_nonexistent_workout(self):
         rv = self.app.get('/workouts/1/')
         assert b'error' in rv.get_data()
+
+    def test_post_workout(self):
+        assert False
 
 
 if __name__ == '__main__':
